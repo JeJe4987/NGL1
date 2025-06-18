dbc2d9a0f51ed1a20c4139dca7036c37bc52336d
JeJe4987 <jepryy6578yz@gmail.com>
2025-06-18 21:47:12 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Replace the below config object with your Firebase project config
+const firebaseConfig = {
+    apiKey: "AIzaSyArc0CMcClGsT__fDKfAj-TuYg5zPoynhw",
+    authDomain: "ngl1-a7cb4.firebaseapp.com",
+    projectId: "ngl1-a7cb4",
+    storageBucket: "ngl1-a7cb4.firebasestorage.app",
+    messagingSenderId: "262815780183",
+    appId: "1:262815780183:web:ae1ee496530c83cc380212",
+    measurementId: "G-HZJGR98R8M"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
